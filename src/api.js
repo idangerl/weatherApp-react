3aa875225b4fec6453434f562aa914ba91a88c90
Oscar Daniel Acho <oscaracho132@gmail.com>
2023-04-04 16:17:21 -0400

    initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "8cb8e6d8b0msh7179ab72cc4ef9ep1dfbd2jsnd86c259866a5",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "ac0d4084897982a882abab3336121aee";
